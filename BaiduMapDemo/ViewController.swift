39b91e5feae93ba60cc9541b0fbe2cac67f4d249
ning <lucky.ning@supernovachina.com>
2018-08-23 10:04:39 +0800

    update

---
+++ BaiduMapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaiduMapDemo
+//
+//  Created by ning on 2018/8/23.
+//  Copyright © 2018年 ning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
